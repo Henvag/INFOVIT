61f3b95ac6d35823c7072c89998e3668820ba060
SpexyRS <48335801+SpexyRS@users.noreply.github.com>
2023-11-09 01:07:53 +0100

    Update hevag2184_Temappgåve 6.py

--- hevag2184_Temappgåve 6.py
+++ hevag2184_Temappgåve 6.py
@@ -30,6 +30,18 @@ Til slutt blir lista returnert.
 
 
 Oppgåve 3:
+I "finn_gjensidige_venner" funksjonen tar den "venneliste" og "navn" som parameter.
+Den hentar lista over vennar ved å bruke "navn" som nøkkelen i "venneliste
+
+Det blir laga ei tom liste for å lagre navna på dei gjensidige vennane.
+
+Vidare itererar "for" loopen igjennom alle personane i "venneliste".
+Det blir sjekka om den gitte personen er i deira "venneliste" og om seg sjølv er i den gitte personen si "venneliste"
+Om begge betingelsane er oppfylt vil personen bli lagt til i lista over gjensidige venner med "append" metoden (https://www.w3schools.com/python/ref_list_append.asp)
+
+Til slutt blir lista returnert
+
+
 
 """
 
@@ -44,7 +56,7 @@ def lag_fortegnelse(personer):
     return fortegnelse
 
 # Funksjonstest
-personer = [('Alice', 25), ('Bob', 30), ('Charlie', 35)]
+personer = [("Alice", 25), ("Bob", 30), ("Charlie", 35)]
 resultat1 = lag_fortegnelse(personer)
 print(resultat1)
 
@@ -66,7 +78,7 @@ def sorter_personer_etter_navn(personer):
 
 
 #Funksjonstest
-personer = {'Alice': 25, 'Charlie': 35, 'Bob': 30}
+personer = {"Alice": 25, "Charlie": 35, "Bob": 30}
 resultat2 = sorter_personer_etter_navn(personer)
 print(resultat2)
 
@@ -79,24 +91,28 @@ print(resultat2)
 
 
 def finn_gjensidige_venner(venneliste, navn):
-    # Henter listen over venner for den gitte personen
+    # Henter lista over venner for personen
     venner = venneliste[navn]
     
-    # Lager en tom liste for å lagre gjensidige venner
+    # Lagar ei tom liste for å lagre gjensidige venner
     gjensidige_venner = []
     
-    # Itererer over alle andre personer i vennelisten
+    # Itererer over alle andre personar i vennelista
     for person, personens_venner in venneliste.items():
-        # Sjekker om den gitte personen er i deres venneliste
+        # Sjekker om den gitte personen er i deira venneliste
         if person != navn and person in venner and navn in personens_venner:
-            # Legger til personen i listen over gjensidige venner
+            # Legger til personen i lista over gjensidige venner
             gjensidige_venner.append(person)
     
     return gjensidige_venner
 
 
 
+
 # Funksjonstest
 
-vennefortegnelse = {'Alice': ['Bob', 'Charlie', 'Devin'], 'Bob': ['Charlie'], 'Charlie': ['Alice'], 'Devin': ['Alice', 'Bob']}
+vennefortegnelse = {"Alice": ["Bob", "Charlie", "Devin"], "Bob": ["Charlie"], "Charlie": ["Alice"], "Devin": ["Alice", "Bob"]}
 print(finn_gjensidige_venner(vennefortegnelse, 'Alice'))
+
+
+
